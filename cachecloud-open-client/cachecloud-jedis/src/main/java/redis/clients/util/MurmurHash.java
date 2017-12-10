72916ef304484238e9144436e1a4700e2e2f07ed
dexuefu <fdexue@gmail.com>
2017-12-10 23:01:10 +0800

    murmurhash

--- cachecloud-open-client/cachecloud-jedis/src/main/java/redis/clients/util/MurmurHash.java
+++ cachecloud-open-client/cachecloud-jedis/src/main/java/redis/clients/util/MurmurHash.java
@@ -15,6 +15,8 @@ import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 
 /**
+ * todo 这是 Redis 使用 MurmurHash2 算法来计算键的哈希值，MurmurHash 算法最初由 Austin Appleby 于 2008 年发明， 这种算法的优点在于， 即使输入的键是有规律的， 算法仍能给出一个很好的随机分布性， 并且算法的计算速度也非常快。
+ todo MurmurHash 算法目前的最新版本为 MurmurHash3 ， 而 Redis 使用的是 MurmurHash2 ， 关于 MurmurHash 算法的更多信息可以参考该算法的主页： http://code.google.com/p/smhasher/ 。
  * This is a very fast, non-cryptographic hash suitable for general hash-based lookup. See
  * http://murmurhash.googlepages.com/ for more details. <br>
  * <p>
