0d6eed2ac986165fa9ac7d3d1197b6296ffe351f
dexuefu <fdexue@gmail.com>
2018-01-16 23:54:10 +0800

    sshutil2

--- cachecloud-open-service/src/main/java/com/sohu/cache/ssh/SSHUtil2.java
+++ cachecloud-open-service/src/main/java/com/sohu/cache/ssh/SSHUtil2.java
@@ -1,7 +1,15 @@
 package com.sohu.cache.ssh;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by jch on 18/1/16.
  */
 public class SSHUtil2 {
+
+    private static final Logger logger = LoggerFactory.getLogger(SSHUtil.class);
+
+    //使用 @SSHTemplate 实现SSHUtil
+    private final static SSHTemplate sshTemplate = new SSHTemplate();
 }
